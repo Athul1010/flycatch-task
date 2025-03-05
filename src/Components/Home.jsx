8d01ed4907269ebf4904e1b9501d6d9774d73631
Athul1010 <athulrajtsuresh1999@gmail.com>
2025-03-06 01:24:57 +0530

    Completed

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -7,7 +7,7 @@ const ItemTypes = {
   WIDGET: 'widget',
 };
 
-const Widget = ({ type, content, index, moveWidget }) => {
+const Widget = ({ type, content, index, moveWidget, updateContent }) => {
   const [{ isDragging }, drag] = useDrag({
     type: ItemTypes.WIDGET,
     item: { index },
@@ -20,17 +20,21 @@ const Widget = ({ type, content, index, moveWidget }) => {
     accept: ItemTypes.WIDGET,
     hover: (draggedItem) => {
       if (draggedItem.index !== index) {
-        moveWidget(draggedItem.index, index);
+        moveWidget(draggedItem.index, index); 
         draggedItem.index = index;
       }
     },
   });
 
+  const handleChange = (e) => {
+    updateContent(index, e.target.value);
+  };
+
   return (
     <div ref={(node) => drag(drop(node))} className={`widget ${isDragging ? 'dragging' : ''}`}>
-      {type === 'Text' && <div>{content}</div>}
+      {type === 'Text' && <input type="text" value={content} onChange={handleChange} />}
       {type === 'Image' && <img src={content} alt="Widget" width="100" />}
-      {type === 'Button' && <button>{content}</button>}
+      {type === 'Button' && <button onClick={() => alert(content)}>{content}</button>}
       {type === 'Table' && (
         <table>
           <tbody>
@@ -81,12 +85,12 @@ const Home = () => {
   const addWidget = (type) => {
     const content =
       type === 'Text'
-        ? 'Text'
+        ? 'Editable Text'
         : type === 'Image'
-          ? react_img
-          : type === 'Button'
-            ? 'Click Me'
-            : [['Row 1', 'Row 2'], ['Row 3', 'Row 4']];
+        ? react_img
+        : type === 'Button'
+        ? 'Click Me'
+        : [['Row 1', 'Row 2'], ['Row 3', 'Row 4']];
     setCanvasWidgets([...canvasWidgets, { type, content }]);
   };
 
@@ -94,6 +98,12 @@ const Home = () => {
     const updatedWidgets = [...canvasWidgets];
     const [moved] = updatedWidgets.splice(from, 1);
     updatedWidgets.splice(to, 0, moved);
+    setCanvasWidgets(updatedWidgets);
+  };
+
+  const updateContent = (index, newContent) => {
+    const updatedWidgets = [...canvasWidgets];
+    updatedWidgets[index].content = newContent;
     setCanvasWidgets(updatedWidgets);
   };
 
@@ -107,26 +117,24 @@ const Home = () => {
   });
 
   return (
-    <div className='container'>
-      <h1 className='builder'>Widget Panel Builder</h1>
-      <div className="home">
-        <div className="widget-panel">
-          <h3>Widgets</h3>
-          {['Text', 'Image', 'Button', 'Table'].map((widget) => (
-            <WidgetItem key={widget} type={widget} />
-          ))}
-        </div>
-        <div className="canvas" ref={drop}>
-          {canvasWidgets.map((widget, index) => (
-            <Widget
-              key={index}
-              index={index}
-              type={widget.type}
-              content={widget.content}
-              moveWidget={moveWidget}
-            />
-          ))}
-        </div>
+    <div className="home">
+      <div className="widget-panel">
+        <h3>Widgets</h3>
+        {['Text', 'Image', 'Button', 'Table'].map((widget) => (
+          <WidgetItem key={widget} type={widget} />
+        ))}
+      </div>
+      <div className="canvas" ref={drop}>
+        {canvasWidgets.map((widget, index) => (
+          <Widget
+            key={index}
+            index={index}
+            type={widget.type}
+            content={widget.content}
+            moveWidget={moveWidget}
+            updateContent={updateContent}
+          />
+        ))}
       </div>
     </div>
   );
