c7369c9c5c65aa745c510e959d4afc745860dab0
Athul1010 <athulrajtsuresh1999@gmail.com>
2025-03-05 23:28:35 +0530

    widget panel builder

---
+++ src/Components/Home.jsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react';
+import { useDrag, useDrop } from 'react-dnd';
+import react_img from '../assets/react-img.jpeg'
+import '../Styles/Home.css';
+
+const ItemTypes = {
+  WIDGET: 'widget',
+};
+
+const Widget = ({ type, content, index, moveWidget }) => {
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.WIDGET,
+    item: { index },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  const [, drop] = useDrop({
+    accept: ItemTypes.WIDGET,
+    hover: (draggedItem) => {
+      if (draggedItem.index !== index) {
+        moveWidget(draggedItem.index, index);
+        draggedItem.index = index;
+      }
+    },
+  });
+
+  return (
+    <div ref={(node) => drag(drop(node))} className={`widget ${isDragging ? 'dragging' : ''}`}>
+      {type === 'Text' && <div>{content}</div>}
+      {type === 'Image' && <img src={content} alt="Widget" width="100" />}
+      {type === 'Button' && <button>{content}</button>}
+      {type === 'Table' && (
+        <table>
+          <tbody>
+            {content.map((row, i) => (
+              <tr key={i}>
+                {row.map((cell, j) => (
+                  <td key={j}>{cell}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+const WidgetItem = ({ type }) => {
+  const [{ isDragging }, drag] = useDrag({
+    type: ItemTypes.WIDGET,
+    item: { type },
+    collect: (monitor) => ({
+      isDragging: !!monitor.isDragging(),
+    }),
+  });
+
+  return (
+    <div ref={drag} className={`widget-panel-item ${isDragging ? 'dragging' : ''}`}>
+      {type}
+    </div>
+  );
+};
+
+const Home = () => {
+  const [canvasWidgets, setCanvasWidgets] = useState([]);
+
+  useEffect(() => {
+    const savedWidgets = JSON.parse(localStorage.getItem('canvasWidgets'));
+    if (savedWidgets) {
+      setCanvasWidgets(savedWidgets);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('canvasWidgets', JSON.stringify(canvasWidgets));
+  }, [canvasWidgets]);
+
+  const addWidget = (type) => {
+    const content =
+      type === 'Text'
+        ? 'Text'
+        : type === 'Image'
+          ? react_img
+          : type === 'Button'
+            ? 'Click Me'
+            : [['Row 1', 'Row 2'], ['Row 3', 'Row 4']];
+    setCanvasWidgets([...canvasWidgets, { type, content }]);
+  };
+
+  const moveWidget = (from, to) => {
+    const updatedWidgets = [...canvasWidgets];
+    const [moved] = updatedWidgets.splice(from, 1);
+    updatedWidgets.splice(to, 0, moved);
+    setCanvasWidgets(updatedWidgets);
+  };
+
+  const [, drop] = useDrop({
+    accept: ItemTypes.WIDGET,
+    drop: (item) => {
+      if (item.index === undefined) {
+        addWidget(item.type);
+      }
+    },
+  });
+
+  return (
+    <div className='container'>
+      <h1 className='builder'>Widget Panel Builder</h1>
+      <div className="home">
+        <div className="widget-panel">
+          <h3>Widgets</h3>
+          {['Text', 'Image', 'Button', 'Table'].map((widget) => (
+            <WidgetItem key={widget} type={widget} />
+          ))}
+        </div>
+        <div className="canvas" ref={drop}>
+          {canvasWidgets.map((widget, index) => (
+            <Widget
+              key={index}
+              index={index}
+              type={widget.type}
+              content={widget.content}
+              moveWidget={moveWidget}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
